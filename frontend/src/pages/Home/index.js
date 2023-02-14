acf190c1a5b7d0cadb2e31679368d443ae131273
letdayy <lcdayane49@gmail.com>
2023-02-14 15:53:28 -0300

    começo da estilização do front

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,7 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+import './style.css';
+
 export default function Home() {
-    return <>
-    <h1>
-        Essa é a home
-    </h1>
-    </>
+    return <div className='body'>
+        <div className='card'>
+            <h1>
+                Personagens
+            </h1>
+
+            <h2>
+                Astra
+            </h2>
+        </div>
+    </div>
 }
