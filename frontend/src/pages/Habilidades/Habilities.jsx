69a0d8e347c7b6e9728be2168430016c4cf67ac7
Paula Dinucci <paulaldinucci@gmail.com>
2023-02-16 10:06:58 -0300

    Add files via upload

--- frontend/src/pages/Habilidades/Habilities.jsx
+++ frontend/src/pages/Habilidades/Habilities.jsx
@@ -1,81 +1,76 @@
-// import React from 'react';
-// import './Habilities.css';
-// import axios from 'axios';
-// import { useEffect, useState} from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState} from 'react';
 // import { Link } from 'react-router-dom';
 
-// export default function Abilities() {
-//     const [Hab,setHabilidades] = useState([])
+export default function Habilities() {
+        const [habilidades,setHabilities] = useState([])
 
-//     useEffect(()=>{
-//         const fetchAllHabilidades = async ()=>{
-//             try{
-//                 const res = await axios.get("http://localhost:8800/habilidades")
-//                 sethabilidades(res.data);
+        useEffect(()=>{
+            const fetchAllHabilities = async ()=>{
+                try{
+                    const res = await axios.get("http://localhost:8800/habilidades")
+                    setHabilities(res.data);
 
-//             }catch(err){
-//                 console.log(err)
-//             }
-//         }
-//         fetchAllhabilidades()
-//     }, [])
+                }catch(err){
+                    console.log(err)
+                }
+            }
+            fetchAllHabilities()
+        }, [])
 
 
-//     const delhabilidades = async (id)=>{
-//         try {
-//             await axios.delete("http://localhost:8800/habilidades/"+id)
-//             window.location.reload()
-//         } catch (err) {
-//             console.log(err)
+
+
+
+    return <div className='painel'>
+        <div className='principal'>
+            <h1>
+                Habilidades
+            </h1>
+
+            <button>
+                +
+            </button>
+
+        </div>
+
+        <div className='main'>
+
+            {habilidades.map((habilidade) => (
+            <div className="personagem" key={habilidade.id}>
+                <h2>
+                    {habilidade.Displacement}
+                </h2>
+                <ul>
+                    <li>
+                        Habilidade: {habilidade.Habilidade}
+                    </li>
+                    <li>
+                        Carregamento máximo: {habilidade.Max_carregamento}
+                    </li>
+                    <li>
+                        Agente: {habilidade.Agente}
+                    </li>
+                    <li>
+                        Funções: {habilidade.Funcoes}
+                    </li>
+                    <li>
+                        Custo: {habilidade.Custo}
+                    </li>
+                </ul>
+                <div className='button'>
+                    <button className='deleteButton' >Deletar</button>
+                    <button className='updateButton'>Atualizar</button>
+                </div>
+            </div>
+            ))}
+
+
+
             
-//         }
-//     }
 
-
-//     return <div className='painel'>
-//         <div className='principal'>
-//         <h1>
-//             Habilidades
-//         </h1>
-
-//         <button>
-//             <Link to="/HabAdd" >
-//             +
-//             </Link>
-//         </button>
-
-//         </div>
-
-//         <div className='main'>
-
-//             {Habilidades.map((habilidade) => (
-//                 <div className="habilidade" key={habilidades.habilidade}>
-//                 <h2>
-//                 {habilidades.agente}
-//             </h2>
-//             <ul>
-//                 <li>
-//                     Habilidade: {personagem.realName}
-//                 </li>
-//                 <li>
-//                     Carregamento máximo: {habilidades.Max_carregamento}
-//                 </li>
-//                 <li>
-//                     Agente: {habilidades.agente}
-//                 </li>
-//                 <li>
-//                     Funções: {habilidades.Funcoes}
-//                 </li>
-//             </ul>
-//             <div className='button'>
-//             <button className='deleteButton' onClick={()=>delHabilidade(Habilidades.habilidade)}>Deletar</button>
-//             <button className='updateButton'>Atualizar</button>
-//             </div>
-//         </div>
-//             ))}
-
-        
-
-//         </div>
-//     </div>
-// }
+        </div>
+    </div>
+}
