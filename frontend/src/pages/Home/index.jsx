fd0f93f51434f7f4b6722af3b02fc4de7cfd1667
Paula Dinucci <paulaldinucci@gmail.com>
2023-02-15 22:03:11 -0300

    Add files via upload

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -26,7 +26,8 @@ export default function Home() {
 
             <button>Armas</button>
 
-            <button>Habilidades</button>
+            <button>
+                <Link to="/Habilities">Habilidades</Link></button>
 
             <button>Mapas</button>
 
